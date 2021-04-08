e512b41a05f1c502f7cd2a7b624775d3456572b1
benwade91 <70988204+benn925@users.noreply.github.com>
2021-04-08 07:56:55 -0700

    comment and reply functions

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -36,28 +36,79 @@ const commentController = {
             .catch(err => res.json(err));
     },
 
+    // add reply
+    addReply({
+        params,
+        body
+    }, res) {
+        Comment.findOneAndUpdate({
+                _id: params.commentId
+            }, {
+                $push: {
+                    replies: body
+                }
+            }, {
+                new: true
+            })
+            .then(commentData => {
+                if (!commentData) {
+                    res.status(404).json({
+                        message: 'No pizza found with this id!'
+                    });
+                    return;
+                }
+                res.json(commentData);
+            })
+            .catch(err => res.json(err))
+    },
+
+
+
     // remove comment
-    removeComment({ params }, res) {
-        Comment.findOneAndDelete({ _id: params.commentId })
-          .then(deletedComment => {
-            if (!deletedComment) {
-              return res.status(404).json({ message: 'No comment with this id!' });
-            }
-            return Pizza.findOneAndUpdate(
-              { _id: params.pizzaId },
-              { $pull: { comments: params.commentId } },
-              { new: true }
-            );
-          })
-          .then(dbPizzaData => {
-            if (!dbPizzaData) {
-              res.status(404).json({ message: 'No pizza found with this id!' });
-              return;
-            }
-            res.json(dbPizzaData);
-          })
-          .catch(err => res.json(err));
-      }
+    removeComment({
+        params
+    }, res) {
+        Comment.findOneAndDelete({
+                _id: params.commentId
+            })
+            .then(deletedComment => {
+                if (!deletedComment) {
+                    return res.status(404).json({
+                        message: 'No comment with this id!'
+                    });
+                }
+                return Pizza.findOneAndUpdate({
+                    _id: params.pizzaId
+                }, {
+                    $pull: {
+                        comments: params.commentId
+                    }
+                }, {
+                    new: true
+                });
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({
+                        message: 'No pizza found with this id!'
+                    });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+
+
+    // remove reply
+    removeReply({params}, res){
+        Comment.findOneAndUpdate({_id: params.commentId},
+            {$pull: {replies: {replyId: params.replyId}}},
+            { new: true})
+            .then(dbCommentData => res.json(dbCommentData)
+            )
+            .catch(err => res.json(err));
+    }
 };
 
 module.exports = commentController;
--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -1,3 +1,5 @@
+// const { post } = require("../../../routes/api/comment-routes");
+
 const $backBtn = document.querySelector('#back-btn');
 const $pizzaName = document.querySelector('#pizza-name');
 const $createdBy = document.querySelector('#created-by');
@@ -9,12 +11,44 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza() {
+  // get id of pizza
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id');
+
+  // get pizzaInfo
+  fetch(`/api/pizzas/${pizzaId}`)
+    .then(response => {
+      // check for a 4xx or 5xx error from server
+      if (!response.ok) {
+        throw new Error({
+          message: 'Something went wrong!'
+        });
+      }
+
+      return response.json();
+    })
+    .then(printPizza)
+    .catch(err => {
+      console.log(err);
+      alert('Cannot find a pizza with this id! Taking you back.');
+      window.history.back();
+    });
+}
+
 function printPizza(pizzaData) {
   console.log(pizzaData);
 
   pizzaId = pizzaData._id;
 
-  const { pizzaName, createdBy, createdAt, size, toppings, comments } = pizzaData;
+  const {
+    pizzaName,
+    createdBy,
+    createdAt,
+    size,
+    toppings,
+    comments
+  } = pizzaData;
 
   $pizzaName.textContent = pizzaName;
   $createdBy.textContent = createdBy;
@@ -86,7 +120,32 @@ function handleNewCommentSubmit(event) {
     return false;
   }
 
-  const formData = { commentBody, writtenBy };
+  const formData = {
+    commentBody,
+    writtenBy
+  };
+
+  fetch(`/api/comments/${pizzaId}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function handleNewReplySubmit(event) {
@@ -105,12 +164,41 @@ function handleNewReplySubmit(event) {
     return false;
   }
 
-  const formData = { writtenBy, replyBody };
+  const formData = {
+    writtenBy,
+    replyBody
+  };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
 }
 
-$backBtn.addEventListener('click', function() {
+$backBtn.addEventListener('click', function () {
   window.history.back();
 });
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+
+getPizza();
